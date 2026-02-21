37130f39865e0a37c40f8123c86aeb90a0df7391
Matias Glasner <mglasner10@gmail.com>
2026-02-21 11:39:17 -0300

    chore(linters): ignorar directorio dist/ en ESLint y Stylelint
    Los archivos minificados en dist/ generaban falsos positivos en
    Stylelint (declaration-block-single-line-max-declarations) y no
    necesitan ser analizados por ningún linter.

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import prettierConfig from 'eslint-config-prettier';
 
 export default [
     {
-        ignores: ['node_modules/'],
+        ignores: ['node_modules/', 'dist/'],
     },
     {
         files: ['js/**/*.js'],
